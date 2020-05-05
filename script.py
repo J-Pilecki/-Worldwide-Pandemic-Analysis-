a528c2e630aca33aaaebff6b325cdc1be192c082
jp431hsu <60199696+jp431hsu@users.noreply.github.com>
2020-05-05 13:40:23 -0700

    Add files via upload

---
+++ script.py
@@ -0,0 +1,14 @@
+import urllib
+from bs4 import BeautifulSoup
+import urllib.request, urllib.parse, urllib.error
+import ssl
+
+cov_url = "https://www.worldometers.info/coronavirus/?utm_campaign=homeAdvegas1?" #url for covid
+
+
+
+
+
+
+
+
