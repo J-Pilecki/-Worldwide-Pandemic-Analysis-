818d22362e5ba69ed616f184c345b83537e4c2fe
jp431hsu <60199696+jp431hsu@users.noreply.github.com>
2020-05-09 15:58:12 -0700

    Update script.py

--- script.py
+++ script.py
@@ -97,12 +97,65 @@ print(covid_CF)
 
 dev_x = ['spanish flu', 'hiv', 'swine flu', 'coronavirus']
 dev_y = [span_CF, hiv_CF, swine_CF, covid_CF]
+plt.xlabel('Pandemics')
+plt.ylabel('Case Fatality')
+plt.title('Pandemic Case Fatality: (deaths/contractions) = CF')
 plt.bar(dev_x, dev_y)
 plt.show()
+
+#source: https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports
+jan21, jan22, jan23, jan24, jan25, jan26, jan27, jan28, jan29, jan30, jan31 = 282, 314, 581, 846, 1320, 2014, 2798, 4593, 6065, 7818, 9826
+dev_janx = ['Jan 21', 'Jan 22', 'Jan 23', 'Jan 24', 'Jan 25', 'Jan 26', 'Jan 27', 'Jan 28', 'Jan 29', 'Jan 30', 'Jan 31']
+dev_jany = [jan21, jan22, jan23, jan24, jan25, jan26, jan27, jan28, jan29, jan30, jan31]
+plt.plot(dev_janx, dev_jany, color = "#444444")
+plt.title('Worldwide Coronavirus cases in January 2020')
+plt.xlabel('Days')
+plt.ylabel('Cases')
+plt.grid(True)
+plt.show()
+
+feb1, feb2, feb3, feb4, feb5, feb6, feb7, feb8, feb9, feb10, feb11, feb12, feb13, feb14, feb15, feb16, feb17, feb18, feb19, feb20, feb21, feb22, feb23, feb24, feb25, feb26, feb27, feb28, feb29 = 11953, 14557, 17391, 20630, 24554, 28276, 31481, 34886, 37558, 40554, 43103, 45171, 46997, 49053, 50580, 51857, 71429, 73332, 75204, 75748, 76769, 77794, 78811, 79331, 80239, 81109, 82294, 83652, 85403
+dev_feb1x = ['Feb 1', 'Feb 2', 'Feb 3', 'Feb 4', 'Feb 5', 'Feb 6', 'Feb 7', 'Feb 8', 'Feb 9', 'Feb 10', 'Feb 11', 'Feb 12', 'Feb 13', 'Feb 14']
+dev_feb1y = [feb1, feb2, feb3, feb4, feb5, feb6, feb7, feb8, feb9, feb10, feb11, feb12, feb13, feb14]
+plt.plot(dev_feb1x, dev_feb1y)
+plt.title('Worldwide Coronavirus cases in February 1 - 14, 2020')
+plt.xlabel('Days')
+plt.ylabel('Cases')
+plt.grid(True)
+plt.show()
+
+dev_feb2x = ['Feb 15', 'Feb 16', 'Feb 17', 'Feb 18', 'Feb 19', 'Feb 20', 'Feb 21', 'Feb 22', 'Feb 23', 'Feb 24', 'Feb 25', 'Feb 26', 'Feb 27', 'Feb 28', 'Feb 29']
+dev_feb2y = [feb15, feb16, feb17, feb18, feb19, feb20, feb21, feb22, feb23, feb24, feb25, feb26, feb27, feb28, feb29]
+plt.plot(dev_feb2x, dev_feb2y)
+plt.title('Worldwide Coronavirus cases in February 15 - 29, 2020')
+plt.xlabel('Days')
+plt.ylabel('Cases')
+plt.grid(True)
+plt.show()
+
+mar1, mar2, mar3, mar4, mar5, mar6, mar7, mar8, mar9, mar10, mar11, mar12, mar13, mar14, mar15, mar16, mar17, mar18, mar19, mar20, mar21, mar22, mar23, mar24, mar25, mar26, mar27, mar28, mar29, mar30, mar31 = 87137, 88948, 90870, 93091, 95324, 98189, 101924, 105586, 109577, 113702, 118319, 125260, 132758, 142534, 153517, 167499, 179104, 191122, 209842, 234073, 266072, 292136, 332924, 372749, 413461, 462680, 509164, 571676, 634825, 693212, 750867
+dev_mar1x = ['Mar 1', 'Mar 2', 'Mar 3', 'Mar 4', 'Mar 5', 'Mar 6', 'Mar 7', 'Mar 8', 'Mar 9', 'Mar 10', 'Mar 11', 'Mar 12', 'Mar 13', 'Mar 14', 'Mar 15']
+dev_mar1y = [mar1, mar2, mar3, mar4, mar5, mar6, mar7, mar8, mar9, mar10, mar11, mar12, mar13, mar14, mar15]
+plt.plot(dev_mar1x, dev_mar1y)
+plt.title('Worldwide Coronavirus cases in March 1 - 15, 2020')
+plt.xlabel('Days')
+plt.ylabel('Cases')
+plt.grid(True)
+plt.show()
+
+dev_mar2x = ['Mar 16', 'Mar 17', 'Mar 18', 'Mar 19', 'Mar 20', 'Mar 21', 'Mar 22', 'Mar 23', 'Mar 24', 'Mar 25', 'Mar 26', 'Mar 27', 'Mar 28', 'Mar 29', 'Mar 30', 'Mar 31']
+dev_mar2y = [mar16, mar17, mar18, mar19, mar20, mar21, mar22, mar23, mar24, mar25, mar26, mar27, mar28, mar29, mar30, mar31]
+plt.plot(dev_mar2x, dev_mar2y)
+plt.title('Worldwide Coronavirus cases in March 16 - 31, 2020')
+plt.xlabel('Days')
+plt.ylabel('Cases')
+plt.grid(True)
+plt.show()
+
+
+
 
 #black_death_total_death = (200000000 - 75000000) / 2
 #print('\nblack death total deaths:\n')
 #print(black_death_total_death)
-
-
 
