b91cd5e2847fbf89768e98bb5d0076a5593aa485
jp431hsu <60199696+jp431hsu@users.noreply.github.com>
2020-05-10 17:53:01 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,28 +1,23 @@
 import urllib
 import requests
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
 import urllib.request, urllib.parse, urllib.error
 import ssl
 from matplotlib import pyplot as plt
 import numpy as np
+from datetime import date
 
 #using requests module access the webpage
 result = requests.get("https://www.worldometers.info/coronavirus/?utm_campaign=homeAdvegas1?")
 #print(result.status_code) #check that website was accessible, 200 = access
 src = result.content #store the page content in a variable
 soup = BeautifulSoup(src)  #create a BS object for later parsing
-#total_cases = soup.find("div", "maincounter-number")
-#total_cases = total_cases.text
+#soup_cases = soup.find("div", "maincounter-number")
+#soup_cases = soup_cases.text
 #print('total cases of covid-19:')
-#print(total_cases)  
+#print(soup_cases)  
 
 all_divs = soup.find_all("div", "maincounter-number")
-#covid_total_cases = all_divs[0]
-#covid_total_deaths = all_divs[1]
-#covid_total_recovered = all_divs[2]
 
 numbers = []
 for i in all_divs:
@@ -158,3 +153,34 @@ labels = ['Japan', 'Thailand', 'Hong Kong', 'South Korea', 'Taiwan', 'Macao', 'U
 plt.pie(slices, labels=labels, autopct='%1.1f%%')
 plt.title("January 23 confirmed cases of Covid-19 outside of China - initial spread")
 plt.show()
+
+# scrape stats so every day the page updates, the graph updates dynamically
+# find html elems that contain date -> find_all to put in []
+# worldwide cases -> find_all to put in []
+# graph the two dynamic vars date and cases
+# unique identifier to key the find_all elems
+
+
+# get an up-to-date list of days in m/d format
+today = date.today()    #today's date
+print("Today's date:", today)
+d1 = str(today.strftime("%m/%d"))  #month/day format 
+print("d1 =", d1)
+
+covid_total_cases = str(covid_total_cases)
+
+txt_dates = []
+#txt_dates.append("05/08")
+dates = []
+with open("dates.txt", "r+") as f:
+    contents = f.readlines()
+    contents = [x.strip() for x in contents]
+if d1 not in contents:
+    with open("dates.txt", "a+") as f:
+        txt_dates.append(d1)   # add today's date to end of list
+        f.write("\n" + d1 + ", " + covid_total_cases)   # write today's date into the file
+for x in contents:
+    print(x)
+
+
+
